1e3fe3759427b1308f15c6d5605f7c37946b2768
bohaohan <54bohaohan@gmail.com>
2015-07-24 23:02:42 +0800

    适配windows

--- app/scripts/nocap.js
+++ app/scripts/nocap.js
@@ -22,9 +22,46 @@ function checkCa(){
     data = data.replace(/\;/g,"*3*");
     data = data.replace(/\+/g,"*4*");
     data = data.replace(/\=/g,"*5*");
-    var url = "http://127.0.0.1:8000/login";
+    var url = "http://fc.ziqiang.net.cn/login";
+    //var url = " http://127.0.0.1:8000/login"
+    $(document).ajaxSend(function(event, xhr, settings) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        function sameOrigin(url) {
+            // url could be relative or scheme relative or absolute
+            var host = document.location.host; // host + port
+            var protocol = document.location.protocol;
+            var sr_origin = '//' + host;
+            var origin = protocol + sr_origin;
+            // Allow absolute or scheme relative URLs to same origin
+            return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+                (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+                // or any other URL that isn't scheme relative or absolute i.e relative.
+                !(/^(\/\/|http:|https:).*/.test(url));
+        }
+        function safeMethod(method) {
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+
+        if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    });
     $.ajax({
-        type:"get",
+        type:"post",
         url:url,
         dataType:"json",
         data: {"img":data},
@@ -35,35 +72,61 @@ function checkCa(){
         },
         error:function(){
             isCheck = false;
-            $("#captcha-img").click();
+            if(ca_retry < 7) {
+                changeCap();
+            }
+            else {
+                alert("好像服务器挂了~建议关闭验证码自动识别~~")
+                isCheck = true;
+            }
+            ca_retry++;
         }
     })
 }
+function changeCap(){
+    var cimg=document.getElementById("captcha-img");
+    cimg.src="/servlet/GenImg?rdddd="+Math.random();
+    isChange = true;
+}
+
 function checkChange(){
-    if(isChange&&!isCheck){
+    if(isChange&&!isCheck&&isOn == "true"){
         isChange = false;
         checkCa();
+    }else if(!isChange&&!isCheck&&isOn == "true"){
+        ca_retry = 0;
     }
-    if(isTry&&!isCheck){
+    if(isTry&&!isCheck&&isOn == "true"){
         isTry = false;
+        retryTimes++;
         login();
+    }else if(!isTry&&!isCheck&&isOn == "true"){
+        retryTimes = 0;
     }
 }
 function login(){
     var id = $("#qxftest").val();
     var pwd = $("input[name = 'pwd']").val();
     var xdvfb = $("input[name = 'xdvfb']").val();
-    var data = "?id="+id+"&pwd="+pwd+"&xdvfb="+xdvfb;
-    var url = "/servlet/Login"
-    if(!isCheck) {
+    localStorage.setItem("id",id);
+    localStorage.setItem("pwd",pwd);
+    var data = "id="+id+"&pwd="+pwd+"&xdvfb="+xdvfb;
+    var url = "/servlet/Login";
+    if(retryTimes>5){
+        alert("仿佛服务器有些问题，建议关闭识别验证码~")
+    }
+    if(!isCheck&&retryTimes<6) {
         $.ajax({
             type: "post",
-            url: url + data,
-            success: function (data) {
+            url: url,
+            data :data,
+            async: false,
+            success: function (data) { 
+                a = data
                 if (data[1] == "!") {
                     window.location = "/stu/stu_index.jsp";
-                } else {
-                    $("#captcha-img").click();
+                }else{
+                    changeCap();
                     isTry = true;
                 }
             },
@@ -76,11 +139,56 @@ function login(){
     }
 }
 
+function setOn(){
+    if($(".isOn").attr("checked") == "checked"){
+        localStorage.setItem("on","true");
+        isOn = true;
+        checkCa();
+       // $("#captcha-img").parent().parent().parent().hide();
+    }else{
+        localStorage.setItem("on","false");
+        isOn = false;
+      //  $("#captcha-img").parent().parent().parent().show();
+    }
+}
+
 $(document).ready(function(){
     isChange = false;
     isTry = false;
     isCheck = false;
-    checkCa();
+    retryTimes = 0;
+    ca_retry = 0;
+    isOn = "true";
+    try{
+        var id = localStorage["id"];
+        var pwd = localStorage["pwd"];
+    }catch(e){
+        id = ""
+        pwd = ""
+    }
+    $("#qxftest").val(id);
+    $("input[name = 'pwd']").val(pwd);
+    try{
+        isOn = localStorage["on"];
+    }catch (e){
+        isOn = true;
+    }
+
+    console.log(isOn);
+    if (isOn == undefined) {
+        isOn = "true";
+    }
+    console.log(isOn);
+    if(isOn == "true") {
+        checkCa();
+    }
+    $("td font").parent().parent().html("<div class='checkbox' style='position: absolute;left: 50%;margin-top: 20px;'><label class='is_on'><input class='isOn' type='checkbox' checked>自动识别验证码</label></div>");
+    if(isOn == "false") {
+        $(".isOn").attr("checked",false)
+    }
+    if(isOn == "true") {
+       // $("#captcha-img").parent().parent().parent().hide();
+    }
     $("#captcha-img").click(function(){
         isChange = true;
     });
@@ -88,8 +196,19 @@ $(document).ready(function(){
         isChange = true;
     });
     $("#loginBtn").click(function(e){
-        e.preventDefault();  //阻止事件冒泡
-        login();
+        if(isOn == "true") {
+            e.preventDefault();  //阻止事件冒泡
+            login();
+        }
     });
+    $(".is_on").click(function(){
+        setOn();
+    })
     setInterval("checkChange()",500);
 })
+
+
+
+
+
+
